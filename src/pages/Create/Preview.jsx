a0938cb14b0393f59cebe2e7051eee26690fe158
Sushan <sushanyadav98@gmail.com>
2021-02-01 13:25:34 +0545

    added feature of creating next resume

--- src/pages/Create/Preview.jsx
+++ src/pages/Create/Preview.jsx
@@ -7,6 +7,7 @@ import FormLayout from "components/Form/FormLayout";
 import Resume from "components/Resume";
 
 import { previewIds } from "constants/formIds";
+import { CREATE } from "constants/routes";
 
 const Preview = ({ history }) => {
   const { FORM_ID } = previewIds;
@@ -18,9 +19,24 @@ const Preview = ({ history }) => {
     resume.save();
   };
 
+  const handleClear = () => {
+    const confirmGoBack = window.confirm(
+      "Do you really want to go back? current progess wont be saved !"
+    );
+
+    if (!confirmGoBack) return;
+
+    localStorage.clear();
+    window.location.reload();
+    history.push(CREATE.BASIC_INFO);
+  };
+
   return (
     <FormLayout headingText="Preview your resume" step={6} formID={FORM_ID}>
       <form id={FORM_ID} onSubmit={handleSubmit}>
+        <button className="text-btn" type="button" onClick={handleClear}>
+          Create another resume
+        </button>
         <div className="resume-wrapper">
           <PDFExport
             fileName="my_resume.pdf"
